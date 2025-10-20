4c5d2c286563b8121422a41a59e2d27b23f4ec78
hugopiramide <hugocastillo.deus@gmail.com>
2025-10-20 23:19:58 +0200

    DoneOC

--- workToDos/src/js/global.js
+++ workToDos/src/js/global.js
@@ -1,15 +1,49 @@
 const userInputId = document.getElementById('userInput');
 const btnEnterId = document.getElementById('enter');
-const ulMainListId = document.getElementById('ulMainList');
+const ulTaskId = document.getElementById('task');
 
-btnEnterId.addEventListener('click', () => btnSend());
+btnEnterId.addEventListener('click', () => sendInput());
+userInputId.addEventListener('keydown',(event) => {
+    if (event.key === 'Enter') {
+            sendInput();
+        }
+    });
 
-const btnSend = () => {
-   const inputValue = userInputId.value.trim();
+const sendInput = () => {
+    createTask();
+    deleteInputText();
+}
+
+const deleteInputText = () => {
+    userInputId.value = '';
+}
+
+const createTask = () => {
+
+    let inputValue = userInputId.value.trim();
+
     if (inputValue !== '') {
+
         let li = document.createElement('li');
         li.innerText = inputValue;
-        ulMainListId.appendChild(li);
-        userInputId.value = '';
+        ulTaskId.appendChild(li);
+    
+        li.addEventListener('click',() => crossOut());
+
+        const crossOut = () => {
+            li.style.background = 'green';
+            btn.style.color = 'white';
+        }
+
+        let btn = document.createElement('button');
+        btn.innerText += 'X';
+        btn.addEventListener('click',() => deleteTask());
+        li.appendChild(btn)
+        
+        const deleteTask = () => {
+            li.remove();
+        }
+
     }
+
 }
