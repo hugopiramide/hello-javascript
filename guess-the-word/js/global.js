8eff046789cddc9c4a9796e4ba4ddd6ae915b2b4
hugopiramide <hugocastillo.deus@gmail.com>
2025-10-22 22:47:32 +0200

    Add random & StartGame features

--- guess-the-word/js/global.js
+++ guess-the-word/js/global.js
@@ -1,4 +1,4 @@
-const guessWords = [
+const keyWords = [
     "platano",
     "rubio",
     "animal",
@@ -13,8 +13,42 @@ const guessWords = [
 const messageId = document.getElementById('message');
 const wordInProgressId = document.getElementById('word-in-progress');
 const guessedLettersId = document.getElementById('guessed-letters')
-const letterId = document.getElementById('letter');
+const letterInputId = document.getElementById('letter');
 const btnGuessId = document.getElementById('guess');
+const btnStartGameId = document.getElementById('play-game');
 const playAgainId = document.getElementById('play-again');
 
-let randomNum;
+let word = '';
+let displayWords = [];
+let guessedWords = [];
+
+btnStartGameId.addEventListener('click', (event) => {
+    event.preventDefault();
+    startGame();       
+})
+
+btnGuessId.addEventListener('click', (event) => {
+    event.preventDefault();
+    guessLetter();
+})
+
+
+const startGame = () => {
+    word = keyWords[generateRandomNumber()];
+    displayWords = Array(word.length).fill("_");
+    wordInProgressId.innerText = displayWords.join(" ");
+    
+    btnStartGameId.classList.add('hide');
+    btnGuessId.classList.remove('hide');
+}
+
+const guessLetter = () => {
+    if(letterInputId.value.length > 1){
+        return '8===)';
+    }
+    
+}
+
+const  generateRandomNumber = () =>  {
+    return Math.floor(Math.random() * keyWords.length);
+}
