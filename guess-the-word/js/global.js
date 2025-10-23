58de59abbf0b46ff2689c6d946f6b43eb7d5c94b
hugopiramide <hugocastillo.deus@gmail.com>
2025-10-23 22:37:28 +0200

    Add funtions guessLetter, startGame & refreshDisplay

--- guess-the-word/js/global.js
+++ guess-the-word/js/global.js
@@ -13,14 +13,19 @@ const keyWords = [
 const messageId = document.getElementById('message');
 const wordInProgressId = document.getElementById('word-in-progress');
 const guessedLettersId = document.getElementById('guessed-letters')
-const letterInputId = document.getElementById('letter');
+const letterInputId = document.getElementById('letter-input');
+const letterLabelId = document.getElementById('letter-label');
 const btnGuessId = document.getElementById('guess');
 const btnStartGameId = document.getElementById('play-game');
 const playAgainId = document.getElementById('play-again');
+const remainingId = document.getElementById('remaining');
+const attemptsId = document.getElementById('attempts');
 
 let word = '';
 let displayWords = [];
-let guessedWords = [];
+let guessWord = [];
+let guessedLetters = 0;
+let attempts = 8;
 
 btnStartGameId.addEventListener('click', (event) => {
     event.preventDefault();
@@ -32,23 +37,58 @@ btnGuessId.addEventListener('click', (event) => {
     guessLetter();
 })
 
-
 const startGame = () => {
     word = keyWords[generateRandomNumber()];
+    guessWord = word.split("");
     displayWords = Array(word.length).fill("_");
-    wordInProgressId.innerText = displayWords.join(" ");
+    refreshDisplay();
     
-    btnStartGameId.classList.add('hide');
+    letterLabelId.classList.remove('hide');
+    letterInputId.classList.remove('hide');
     btnGuessId.classList.remove('hide');
+    btnStartGameId.classList.add('hide');
 }
 
 const guessLetter = () => {
     if(letterInputId.value.length > 1){
-        return '8===)';
+        guessedLettersId.innerText = "Introduce only 1 letter";
+        return;
     }
+    guessedLettersId.innerText = "";
+
+    let i;
+    let countAttempt = true;
+    for(i = 0; i < guessWord.length; i++){
+        if(guessWord[i] == letterInputId.value){
+            guessedLettersId.innerText = "You got it !!";
+            displayWords[i] = letterInputId.value;
+            countAttempt = false;
+        }
+    }
+    if(countAttempt){
+        attempts--;   
+        if(attempts <= 0){
+            endGame();
+            return;
+        }
+    }
+
+    refreshDisplay();
+}
+
+const endGame = () => {
     
 }
 
-const  generateRandomNumber = () =>  {
+const generateRandomNumber = () =>  {
     return Math.floor(Math.random() * keyWords.length);
+}
+
+const refreshDisplay = () => {
+    wordInProgressId.innerText = displayWords.join(" ");
+    if(attempts == 1){
+        attemptsId.innerText = attempts +  " guess";
+    }else{
+        attemptsId.innerText = attempts + " guesses";
+    }
 }
